21a27afbdd0d777525d07a49237f7f159a66b00b
SemyonYa <mygarbage86@mail.ru>
2019-12-05 17:28:22 +0300

    доработки мелкие

--- frontend/views/good/company.php
+++ frontend/views/good/company.php
@@ -7,51 +7,50 @@ use frontend\models\ImageOverride;
 <div class="eda-company-wrap">
     <div class="eda-company">
         <div class="eda-company-description">
-            <h1><?=$farmer->name?></h1>
-            <p><?=$farmer->description?></p>
+            <h1><?= $farmer->name ?></h1>
+            <p><?= $farmer->description ?></p>
             <hr>
-            <p>Минимальная стоимость заказа: <b><?=$farmer->min_cost?> руб.</b></p>
+            <p>Минимальная стоимость заказа: <b><?= $farmer->min_cost ?> руб.</b></p>
             <p>
-                <button type="button" class="btn btn-delivery" data-toggle="popover" title="" data-content="<?= $farmer->delivery ?>">Информация о доставке</button>
+                <button type="button" class="btn btn-delivery" data-toggle="popover" title="" data-content="<?= ($farmer->delivery) ?: 'Нет информации' ?>">Информация о доставке</button>
             </p>
             <hr>
             <div class="eda-company-links">
-                <div class="eda-company-about-link" onclick="GoTo('/farmer/promos?farmer_id=<?=$farmer->id?>')">
+                <div class="eda-company-about-link" onclick="GoTo('/farmer/promos?farmer_id=<?= $farmer->id ?>')">
                     <span class="glyphicon glyphicon-gift"></span>
                     <span> Акции</span>
                 </div>
-                <div class="eda-company-about-link" onclick="GoTo('/farmer/videos?farmer_id=<?=$farmer->id?>')">
+                <div class="eda-company-about-link" onclick="GoTo('/farmer/videos?farmer_id=<?= $farmer->id ?>')">
                     <span class="glyphicon glyphicon-play"></span>
                     <span> Видео </span>
                 </div>
-                <div class="eda-company-about-link" onclick="GoTo('/farmer/posts?farmer_id=<?=$farmer->id?>')">
+                <div class="eda-company-about-link" onclick="GoTo('/farmer/posts?farmer_id=<?= $farmer->id ?>')">
                     <span class="glyphicon glyphicon-pencil"></span>
                     <span> Блог </span>
                 </div>
             </div>
         </div>
         <div class="eda-company-ava">
-            <img src="<?=ImageOverride::getPath($farmer->poster)?>"/>
+            <img src="<?= ImageOverride::getPath($farmer->poster) ?>" />
         </div>
-        <hr style="flex-basis: 100%; border-color: #1f7b1e"/>
+        <hr style="flex-basis: 100%; border-color: #1f7b1e" />
         <div class="eda-company-goods">
             <h3 onclick="FilteringCompanyGoods()">Продукты компании</h3>
             <div class="eda-company-goods-filters">
                 <div class="eda-company-goods-filters-all-checked" title="выбрать все" onclick="AllChecked()"></div>
                 <div class="eda-company-goods-filters-items">
-                    <?php foreach ($categories as $category): ?>
-                        <label class="eda-company-goods-filters-item" for="f1<?=$category->id?>">
-                            <input data-id="<?=$category->id?>" checked type="checkbox" name="flt_category"
-                                   id="f1<?=$category->id?>" onchange="FilteringCompanyGoods()"/>
-                            <?=$category->name?>
+                    <?php foreach ($categories as $category) : ?>
+                        <label class="eda-company-goods-filters-item" for="f1<?= $category->id ?>">
+                            <input data-id="<?= $category->id ?>" checked type="checkbox" name="flt_category" id="f1<?= $category->id ?>" onchange="FilteringCompanyGoods()" />
+                            <?= $category->name ?>
                         </label>
-                    <?php endforeach;?>
+                    <?php endforeach; ?>
                 </div>
                 <div class="eda-company-goods-filters-all-unchecked" title="отменить выбор всех" onclick="AllUnchecked()"></div>
             </div>
 
             <div id="CompanyGoodList" class="eda-company-good-list">
-                <!--                ajax-->
+                <!-- ajax -->
                 <span><b class="text-success"> Загрузка... </b></span>
             </div>
         </div>
@@ -60,7 +59,12 @@ use frontend\models\ImageOverride;
 
 <script>
     $('#CompanyGoodList').load('/good/farmer-good-list?farmer_id=<?=$farmer->id?>');
-    $(function () {
-        $('[data-toggle="popover"]').popover()
+
+    $(function() {
+        console.log((window.innerWidth > window.innerHeight) ? 'right' : 'bottom');
+        $('[data-toggle="popover"]').popover({
+            trigger: 'hover',
+            placement: (window.innerWidth > window.innerHeight) ? 'auto right' : 'auto top'
+        })
     })
 </script>
--- frontend/views/site/cart-inner.php
+++ frontend/views/site/cart-inner.php
@@ -3,25 +3,25 @@
 /* @var $cart_good_item \frontend\models\CartGoodItem */
 ?>
 
-<?php if (count($farmer_items) > 1): ?>
+<?php if (count($farmer_items) > 1) : ?>
     <div class="alert alert-warning alert-dismissable">
         <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
         <p><strong>Внимание!</strong></p>
         <p>Вы добавили в корзину товары с нескольких фермерских хозяйств. Просим обратить внимание, что
             будет
-            сформировано <?=count($farmer_items)?> заказа(ов), у каждого из которых свои срок и место доставки.</p>
+            сформировано <?= count($farmer_items) ?> заказа(ов), у каждого из которых свои срок и место доставки.</p>
     </div>
-<?php endif;?>
-<?php if (count($farmer_items) > 0): ?>
-    <?php $sum = 0;?>
-    <?php $is_valid_sum = true;?>
-    <?php foreach ($farmer_items as $farmer_item): ?>
-        <h3><?=$farmer_item->farmer->name?></h3>
-        <?php if (!$farmer_item->isValidSum()): ?>
+<?php endif; ?>
+<?php if (count($farmer_items) > 0) : ?>
+    <?php $sum = 0; ?>
+    <?php $is_valid_sum = true; ?>
+    <?php foreach ($farmer_items as $farmer_item) : ?>
+        <h3><?= $farmer_item->farmer->name ?></h3>
+        <?php if (!$farmer_item->isValidSum()) : ?>
             <div class="alert alert-danger">
-                Минимальная сумма заказа - <?=$farmer_item->farmer->min_cost?> руб.
+                Минимальная сумма заказа - <?= $farmer_item->farmer->min_cost ?> руб.
             </div>
-        <?php endif;?>
+        <?php endif; ?>
         <table class="table table-hover eda-cart-table">
             <thead>
                 <tr>
@@ -38,56 +38,55 @@
             </thead>
             <tbody>
                 <?php
-$i = 1;
-?>
-                <?php foreach ($farmer_item->cart_good_items as $cart_good_item): ?>
+                        $i = 1;
+                        ?>
+                <?php foreach ($farmer_item->cart_good_items as $cart_good_item) : ?>
                     <tr>
-                        <td><?=$i++?></td>
-                        <td data-toggle="modal" data-target="#GoodModal" class="eda-cart-table-goodname" onclick="LoadGoodModalSearch(<?=$cart_good_item->good->id?>)"><?=$cart_good_item->good->name?></td>
-                        <td><?=$cart_good_item->good->price?> </td>
+                        <td><?= $i++ ?></td>
+                        <td data-toggle="modal" data-target="#GoodModal" class="eda-cart-table-goodname" onclick="LoadGoodModalSearch(<?= $cart_good_item->good->id ?>)"><?= $cart_good_item->good->name ?></td>
+                        <td><?= $cart_good_item->good->price ?> </td>
                         <td>
-                            <input min="1" class="form-control" type="number" value="<?=$cart_good_item->quantity?>" oninput="EditCartQuantity(this)" data-farmerid="<?=$farmer_item->farmer->id?>" data-goodid="<?=$cart_good_item->good->id?>" />
+                            <input min="1" class="form-control" type="number" value="<?= $cart_good_item->quantity ?>" oninput="EditCartQuantity(this)" data-farmerid="<?= $farmer_item->farmer->id ?>" data-goodid="<?= $cart_good_item->good->id ?>" />
                         </td>
-                        <td><?=$cart_good_item->getSum()?></td>
-                        <td><span class="glyphicon glyphicon-remove eda-cart-remove" data-goodid="<?=$cart_good_item->good->id?>" data-farmerid="<?=$farmer_item->farmer->id?>" data-goodname="<?=$cart_good_item->good->name?>" title="Удалить товар из корзины" onclick="RemoveGoodFromCart(this)"></span></td>
+                        <td><?= $cart_good_item->getSum() ?></td>
+                        <td><span class="glyphicon glyphicon-remove eda-cart-remove" data-goodid="<?= $cart_good_item->good->id ?>" data-farmerid="<?= $farmer_item->farmer->id ?>" data-goodname="<?= $cart_good_item->good->name ?>" title="Удалить товар из корзины" onclick="RemoveGoodFromCart(this)"></span></td>
                     </tr>
-                <?php endforeach;?>
+                <?php endforeach; ?>
             </tbody>
             <tfoot>
                 <tr>
                     <td colspan="4">ИТОГО</td>
-                    <td colspan="2"><?=$farmer_item->getSum()?></td>
-                    <?php $sum += $farmer_item->getSum();?>
-                    <?php $is_valid_sum = $is_valid_sum && $farmer_item->isValidSum();?>
-                </tr>
-                <tr>
-                    <td colspan="5">
-                        <p>Дата доставки: <?=($farmer_item->farmer->getNextDay()) ? $farmer_item->farmer->getNextDay() : '-'?></p>
-                        <p>Адрес доставки: Кутузовский проспект, д. 152, корп. 3, лит. Г</p>
-                        <p>
-                            <button type="button" class="btn btn-delivery" data-toggle="popover" title="" data-content="<?=$farmer_item->farmer->delivery?>">Информация о доставке</button>
-                        </p>
-                    </td>
+                    <td colspan="2"><?= $farmer_item->getSum() ?></td>
+                    <?php $sum += $farmer_item->getSum(); ?>
+                    <?php $is_valid_sum = $is_valid_sum && $farmer_item->isValidSum(); ?>
                 </tr>
             </tfoot>
         </table>
-    <?php endforeach;?>
-    <?php if (count($farmer_items) > 1): ?>
-        <h2>Общая сумма заказа составляет <span><?=$sum?> &#8381;</span></h2>
-    <?php endif;?>
+        <div class="eda-cart-delivery">
+            <button type="button" class="btn btn-delivery" data-toggle="popover" title="" data-content="<?= ($farmer_item->farmer->delivery) ?: 'Нет информации' ?>">Условия доставки</button>
+            <p>Дата доставки: <?= ($farmer_item->farmer->getNextDay()) ? $farmer_item->farmer->getNextDay() : '-' ?></p>
+        </div>
+        <!-- <p>Адрес доставки: Кутузовский проспект, д. 152, корп. 3, лит. Г</p> -->
+
+    <?php endforeach; ?>
+    <?php if (count($farmer_items) > 1) : ?>
+        <h2>Общая сумма заказа составляет <span><?= $sum ?> &#8381;</span></h2>
+    <?php endif; ?>
     <p class="eda-btn-submit">
-        <button id="OrderSubmit" type="button" class="btn btn-submit-order" data-toggle="modal" data-target="#OrderModal" <?=($is_valid_sum) ?: 'disabled'?>>Подтвердить</button>
+        <button id="OrderSubmit" type="button" class="btn btn-submit-order" data-toggle="modal" data-target="#OrderModal" <?= ($is_valid_sum) ?: 'disabled' ?>>Подтвердить</button>
     </p>
-<?php else: ?>
+<?php else : ?>
     <div class="alert alert-warning">
         Корзина пуста!
     </div>
-<?php endif;?>
+<?php endif; ?>
 
 <script>
-    $(function () {
+    $(function() {
+        console.log((window.innerWidth > window.innerHeight) ? 'right' : 'bottom');
         $('[data-toggle="popover"]').popover({
-            trigger: 'focus'
+            trigger: 'hover',
+            placement: (window.innerWidth > window.innerHeight) ? 'auto right' : 'auto top'
         })
     })
 </script>
